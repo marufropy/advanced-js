c06a54da7e3ded927a617d00da8b61c9901e18fb
marufropy <marufropy@gmail.com>
2020-08-17 17:50:52 +0600

    filter

--- basics.js
+++ basics.js
@@ -49,16 +49,19 @@
 // }
 
 const numbers = [1, 2, 3, 4, 5];
-const result1 = [];
+// const result1 = [];
 
-for(let i = 0; i < numbers.length; i++){
-    result1.push(numbers[i] * numbers[i]);
-}
+// for(let i = 0; i < numbers.length; i++){
+//     result1.push(numbers[i] * numbers[i]);
+// }
 
-const result2 = numbers.map(function(element){
-    return element * element;
-})
+// const result2 = numbers.map(function(element){
+//     return element * element;
+// })
 
-const result3 = numbers.map(x => x * x);
+// const result3 = numbers.map(x => x * x);
 
-console.log(numbers, result1, result2, result3);
+// console.log(numbers, result1, result2, result3);
+
+const bigger = numbers.filter(x => x > 3);
+console.log(bigger);
