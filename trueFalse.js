f2ad4e86cb4da669148032bf732d5044f6b72592
marufropy <marufropy@gmail.com>
2020-08-17 18:16:16 +0600

    files separated

--- trueFalse.js
+++ trueFalse.js
@@ -16,12 +16,14 @@ if(first == second){
 else{
     console.log("false");
 }
+
 if(first == third){
     console.log("true");
 }
 else{
     console.log("false");
 }
+
 if(second == third){
     console.log("true");
 }
@@ -29,42 +31,24 @@ else{
     console.log("false");
 }
 
+
 if(first === second){
     console.log("true");
 }
 else{
     console.log("false");
 }
+
 if(first === third){
     console.log("true");
 }
 else{
     console.log("false");
 }
+
 if(second === third){
     console.log("true");
 }
 else{
     console.log("false");
 }
-
-const numbers = [1, 2, 3, 4, 5];
-const result1 = [];
-
-for(let i = 0; i < numbers.length; i++){
-    result1.push(numbers[i] * numbers[i]);
-}
-
-const result2 = numbers.map(function(element){
-    return element * element;
-})
-
-const result3 = numbers.map(x => x * x);
-
-console.log(numbers, result1, result2, result3);
-
-const bigger = numbers.filter(x => x > 3);
-console.log(bigger);
-
-const element = numbers.find(x => x > 3);
-console.log(element);
