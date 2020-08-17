0e836443591268c7e7c74423b21b9aba98517040
marufropy <marufropy@gmail.com>
2020-08-17 17:05:50 +0600

    == vs ===

--- basics.js
+++ basics.js
@@ -2,5 +2,46 @@
 //0, "", undefined, null, NaN, false
 
 //truthy
-//"0", " ", [], true
+//1, "0", " ", [], true
 
+//null vs undefined
+
+const first = 1;
+const second = "1";
+const third = true;
+if(first == second){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+if(first == third){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+if(second == third){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+if(first === second){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+if(first === third){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+if(second === third){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
