0e5fcd676cb49ce2dec7965d9a986f09ac74a8db
marufropy <marufropy@gmail.com>
2020-08-17 17:47:09 +0600

    map

--- basics.js
+++ basics.js
@@ -6,42 +6,59 @@
 
 //null vs undefined
 
-const first = 1;
-const second = "1";
-const third = true;
-if(first == second){
-    console.log("true");
+// const first = 1;
+// const second = "1";
+// const third = true;
+
+// if(first == second){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+// if(first == third){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+// if(second == third){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(first === second){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+// if(first === third){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+// if(second === third){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+const numbers = [1, 2, 3, 4, 5];
+const result1 = [];
+
+for(let i = 0; i < numbers.length; i++){
+    result1.push(numbers[i] * numbers[i]);
 }
-else{
-    console.log("false");
-}
-if(first == third){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-if(second == third){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-if(first === second){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-if(first === third){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-if(second === third){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
+
+const result2 = numbers.map(function(element){
+    return element * element;
+})
+
+const result3 = numbers.map(x => x * x);
+
+console.log(numbers, result1, result2, result3);
